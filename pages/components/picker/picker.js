51019bb5a267440cec16176931624b2370c0f40b
hexiaoqiang <hexiaoqiang@cketrip.com>
2018-09-27 17:43:10 +0800

    添加picker选择

--- pages/components/picker/picker.js
+++ pages/components/picker/picker.js
@@ -1,5 +1,7 @@
 Page({
   data: {
+    title:'picker选择器',
+    content:'点击蒙板可以取消选择，点击取消亦可，点击确定，返回选中的项目的索引。选中框的颜色可以改变，可以多列选择，传入需要选择的数据即可。',
     Citys:[
       1,2,3,4,5
     ],
@@ -7,7 +9,9 @@ Page({
       name:'自定义',
       mask:false
     }],
+    pickerId:0,
     ActiveId:0,
+
   },
   onLoad: function (options) {
 
@@ -18,18 +22,28 @@ Page({
        Id = e.currentTarget.dataset.id;
     pickerType[Id].mask = true;
     this.setData({
-      pickerType
+      pickerType,
+      pickerId:Id
     })
   },
   onConfirm(e){
-    let ActiveId = e.detail;
+    let that = this,
+      pickerId = that.data.pickerId,
+      pickerType = that.data.pickerType,
+      ActiveId = e.detail;
+    pickerType[pickerId].mask = false;
     this.setData({
-      ActiveId
+      ActiveId,
+      pickerType
     })
   },
   onClose(){
-    this.setData({
-      mask:false
+    let that = this,
+      pickerId = that.data.pickerId,
+      pickerType = that.data.pickerType;
+    pickerType[pickerId].mask = false;
+    that.setData({
+      pickerType
     })
   }
 })
